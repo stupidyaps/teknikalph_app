44ff3a094f2d5ffc6941df45fdcae9fcf2ba291d
Rafael Yapchiongco <jr.yapchiongco@gmail.com>
2022-03-16 16:28:36 +0800

    major update Login Page Design

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -7,6 +8,35 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage> {
   bool _rememberMe = false;
+
+  Widget _createLogo(){
+    return Container(
+        padding: const EdgeInsets.fromLTRB(0,10,0,0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: const BoxDecoration(
+                // shape: BoxShape.circle,
+                // color: Colors.white,
+                // boxShadow: [
+                //   BoxShadow(
+                //     color: Colors.black26,
+                //     offset: Offset(0,2),
+                //     blurRadius: 6.0,
+                //   )
+                // ],
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo.png'),
+                  )
+              ),
+            )
+          ],
+        )
+    );
+  }
 
   Widget _createUserNameBox(){
     return Column(
@@ -18,6 +48,7 @@ class _LoginPageState extends State<LoginPage> {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.black),
             boxShadow: const [
               BoxShadow(
                 color: Colors.black12,
@@ -37,7 +68,7 @@ class _LoginPageState extends State<LoginPage> {
               contentPadding: EdgeInsets.only(top: 15.0),
               prefixIcon: Icon(
                 Icons.person,
-                color: Color(0xff9f5074),
+                color: Color(0xff000000),
               ),
               hintText: "Username",
               hintStyle: TextStyle(
@@ -60,6 +91,7 @@ class _LoginPageState extends State<LoginPage> {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.black),
             boxShadow: const [
               BoxShadow(
                 color: Colors.black12,
@@ -74,19 +106,20 @@ class _LoginPageState extends State<LoginPage> {
             keyboardType: TextInputType.text,
             style: TextStyle(
               color: Color(0xff522971),
-
             ),
             decoration: InputDecoration(
               border: InputBorder.none,
               contentPadding: EdgeInsets.only(top: 15.0),
               prefixIcon: Icon(
                 Icons.lock,
-                color: Color(0xffd56c76),
+                color: Color(0xff000000),
               ),
               hintText: "Password",
               hintStyle: TextStyle(
                 color: Colors.black45,
               ),
+              suffixIcon: InkWell(
+                  child: Icon(Icons.visibility)),
             ),
           ),
         )
@@ -94,31 +127,17 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _createFPassBtn(){
-    return Container(
-      alignment: Alignment.centerRight,
-      child: TextButton(
-          onPressed: () => debugPrint('Forgot Password Button Pressed'),
-          child: const Text("Forgot Password?",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          )
-      ),
-    );
-  }
-
-  Widget _createRMeCheckBox(){
+  Widget _createRMeFPass(){
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Theme(
             data: ThemeData(
-                unselectedWidgetColor: Colors.white),
+                unselectedWidgetColor: Colors.black),
             child: Checkbox(
                 value: _rememberMe,
-                checkColor: const Color(0xffe07e77),
-                activeColor: Colors.white,
+                checkColor: const Color(0xffffffff),
+                activeColor: Colors.black,
                 onChanged: (value) {
                   setState(() {
                     _rememberMe = value!;
@@ -128,30 +147,42 @@ class _LoginPageState extends State<LoginPage> {
         ),
         const Text("Remember Me",
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
-        )
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(40.0,0,0,0),
+          child: TextButton(
+              onPressed: () => debugPrint('Forgot Password Button Pressed'),
+              child: const Text("Forgot Password?",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+          ),
+        ),
       ],
     );
   }
 
   Widget _createLoginBtn(){
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
       width: double.infinity,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
             elevation: 2,
-            primary: Colors.white,
+            primary: Colors.black,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30.0),
             ),
             padding: const EdgeInsets.all(12.0)
         ),
-        child: const Text("LOGIN",
+        child: const Text("Sign In",
           style: TextStyle(
-            color: Color(0xffe88b78),
+            color: Color(0xffffffff),
             fontSize: 18,
             letterSpacing: 1.5,
             fontWeight: FontWeight.bold,
@@ -166,17 +197,9 @@ class _LoginPageState extends State<LoginPage> {
     return Column(
       children: const <Widget>[
         Text(
-            "- OR -",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
-            )
-        ),
-        SizedBox(height: 20.0),
-        Text(
-          "Log in with",
+          "or",
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
         )
@@ -185,75 +208,58 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _createAltSignInBtn(){
-    return Padding(padding: const EdgeInsets.all(10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            GestureDetector(
-                onTap: () => debugPrint("Login with Google Button Tapped"),
-                child: Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0,2),
-                          blurRadius: 6.0,
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage('assets/google.png'),
-                      )
-                  ),
-                )
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+            elevation: 2,
+            primary: Colors.black,
+            onPrimary: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
             ),
-            GestureDetector(
-                onTap: () => debugPrint("Login with Facebook Button Tapped"),
-                child: Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0,2),
-                          blurRadius: 6.0,
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage('assets/facebook.jpg'),
-                      )
+            padding: const EdgeInsets.all(12.0)
+        ),
+        label: const Text("Sign In with Google",
+          style: TextStyle(
+            fontSize: 18,
+            letterSpacing: 1.5,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        icon: const FaIcon(FontAwesomeIcons.google, color: Colors.white,),
+        onPressed: () => debugPrint("Google Sign In Button Tapped"),
+      ),
+
+    );
+  }
+
+  Widget _createSignUpBtn(){
+    return GestureDetector(
+      onTap: () => debugPrint("Sign Up button tapped"),
+      child: RichText(
+          text: const TextSpan(
+              children: [
+                TextSpan(
+                  text: "Don't have an Account? ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w400,
                   ),
-                )
-            ),
-            GestureDetector(
-                onTap: () => debugPrint("Login with Twitter Button Tapped"),
-                child: Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0,2),
-                          blurRadius: 6.0,
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage('assets/twitter.png'),
-                      )
+                ),
+                TextSpan(
+                  text: "Sign Up",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                )
-            ),
-          ],
-        )
+                ),
+              ]
+          )
+      ),
     );
   }
 
@@ -270,9 +276,9 @@ class _LoginPageState extends State<LoginPage> {
                   begin: Alignment.topCenter,
                   end:  Alignment.bottomCenter,
                   colors: [
-                    Color(0xFF3a1c71),
-                    Color(0xFFd76d77),
-                    Color(0xFFffaf7b),
+                    Color(0xFFffffff),
+                    Color(0xFFffffff),
+                    Color(0xFFffffff),
                   ],
                 )
             ),
@@ -280,33 +286,38 @@ class _LoginPageState extends State<LoginPage> {
           SizedBox(
             height: double.infinity,
             child: SingleChildScrollView(
-              physics: const AlwaysScrollableScrollPhysics(),
               padding: const EdgeInsets.symmetric(
-                  horizontal: 40.0,
-                  vertical: 100.0
+                  horizontal: 50.0,
+                  vertical: 25.0
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  const Text(
-                      "Welcome, Yaps",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
+                  _createLogo(),
+                  const SizedBox(height: 10.0),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child:
+                      const Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       )
                   ),
-                  const SizedBox(height: 30.0),
                   _createUserNameBox(),
                   const SizedBox(height: 15.0),
                   _createPasswordBox(),
-                  _createFPassBtn(),
-                  _createRMeCheckBox(),
+                  _createRMeFPass(),
                   _createLoginBtn(),
-                  const SizedBox(height: 10.0),
+                  const SizedBox(height: 15.0),
                   _createOrSignInWithTxt(),
-                  const SizedBox(height: 10.0),
+                  const SizedBox(height: 5.0),
                   _createAltSignInBtn(),
+                  const SizedBox(height: 13.0),
+                  _createSignUpBtn(),
                 ],
               ),
             ),
