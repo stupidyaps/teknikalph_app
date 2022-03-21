0cf9eca16173955bdc7fe1f0ae3123c22d32a9b8
Rafael Yapchiongco <jr.yapchiongco@gmail.com>
2022-03-21 17:41:50 +0800

    created home page for navigation

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:teknikalph_app/ui/tradesmen_page.dart';
-// import 'package:material_floating_search_bar/material_floating_search_bar.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:teknikalph_app/ui/favorites_page.dart';
+import 'package:teknikalph_app/ui/orders_page.dart';
+import 'package:teknikalph_app/ui/services_page.dart';
+import 'package:teknikalph_app/ui/settings_page.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
@@ -12,249 +13,53 @@ class HomePage extends StatefulWidget{
 
 //DAI PA NAKAKASEARCH, TEMPLATE PALANG
 class _HomePageState extends State<HomePage> {
-  Widget _createSearchBox(){
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30.0),
-            border: Border.all(color: Colors.black),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 6.0,
-                offset: Offset(0, 2),
-              ),
-            ],
-          ),
-          height: 55.0,
-          child: const TextField(
-            keyboardType: TextInputType.text,
-            style: TextStyle(
-              color: Color(0xff000000),
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 15.0),
-              prefixIcon: Icon(
-                Icons.search,
-                color: Color(0xff000000),
-              ),
-              hintText: "Search",
-              hintStyle: TextStyle(
-                color: Colors.black45,
-              ),
-            ),
-          ),
-        )
-      ],
-    );
-  }
+  int _currentIndex = 0;
 
-  // TEMPORARILY WILL REDIRECT TO ONE PAGE ONLY -> TRADESMEN PAGE
-  Widget _createServiceBtn(String sName){
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      width: double.infinity,
-      height: 100,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            elevation: 2,
-            primary: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-              side: const BorderSide(
-                  width: 1.0,
-                  color: Colors.black
-              ),
-            ),
-          alignment: Alignment.bottomLeft,
-          padding: const EdgeInsets.only(bottom: 10, left: 10),
-        ),
-        child: Text(sName,
-          textAlign: TextAlign.left,
-          style: const TextStyle(
-            color: Color(0xff000000),
-            fontSize: 18,
-            letterSpacing: 1.5,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const TradesmenPage()),
-          );
-        },
-      ),
-    );
-  }
+  PageController _pageController = PageController(initialPage: 0);
 
-  //For User Picture
-  Widget _createUserPic(){
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-                alignment: Alignment.centerLeft,
-                child:
-                const Text(
-                  "Services",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-            ),
-            const SizedBox(width:150),
-            GestureDetector(
-            onTap: () => debugPrint("User Profile Tapped"),
-            child: Container(
-              height: 60.0,
-              width: 60.0,
-              decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  offset: Offset(0,2),
-                  blurRadius: 6.0,
-                )
-              ],
-                image: DecorationImage(
-                 image: AssetImage('assets/defaultUserImage.png'),
-                )
-              ),
-            )
-        ),
-      ],
-    );
-  }
+  final _bottomNavigationBarItems = [
+    const BottomNavigationBarItem(icon: Icon(Icons.home), label: "Services",),
+    const BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: "Orders",),
+    const BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites",),
+    const BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings",),
+  ];
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end:  Alignment.bottomCenter,
-                  colors: [
-                    Color(0xFFffffff),
-                    Color(0xFFffffff),
-                    Color(0xFFffffff),
-                  ],
-                )
-            ),
-          ),
-          SizedBox(
-            height: double.infinity,
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 30.0,
-                  vertical: 70.0
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  _createUserPic(),
-                  _createSearchBox(),
-                  const SizedBox(height: 15.0,),
-                  Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Plumbing")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Carpentry")),
-                        ],
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Cleaning")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Shopping")),
-                        ],
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Cooking")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Yardwork")),
-                        ],
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Mechanical")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Electrical")),
-                        ],
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Bills")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Gardening")),
-                        ],
-                      ),
-                      const SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(child: _createServiceBtn("Carpentry")),
-                          const SizedBox(width: 15.0),
-                          Expanded(child: _createServiceBtn("Carpentry")),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Services",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: "Orders",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: "Favorites",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          ),
-        ],
-        onTap: (int i) => debugPrint("Button $i Tapped"),
-      ),
-    );
+     return Scaffold(
+       body: PageView(
+         controller: _pageController,
+         onPageChanged: (newIndex){
+           setState(() {
+             _currentIndex = newIndex;
+           });
+         },
+         children: const [
+           ServicesPage(),
+           OrdersPage(),
+           FavoritesPage(),
+           SettingsPage(),
+         ],
+       ),
+       bottomNavigationBar: Container(
+         decoration: const BoxDecoration(
+             boxShadow: <BoxShadow>[
+               BoxShadow(
+                   color: Colors.black54,
+                   blurRadius: 10.0,
+                   offset: Offset(0.0,0.75)
+               )
+             ]
+         ),
+         child: BottomNavigationBar(
+            items: _bottomNavigationBarItems,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              _pageController.animateToPage(index, duration: const Duration(milliseconds: 500), curve: Curves.ease);
+            },
+         ),
+       ),
+     );
   }
 }
